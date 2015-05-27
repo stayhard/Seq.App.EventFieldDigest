2f03279c58b1081bd513bf104d89e605ac9b39f5
Christopher Lindblom <hello@lindblom.co>
2015-05-27 14:43:59 +0200

    Initial commit

---
+++ Seq.App.EventFieldDigest/Digest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Seq.App.EventFieldDigest
+{
+    public class Digest
+    {
+        private readonly Regex _ignoreMatcher;
+        private readonly Regex _sanitizationMatcher;
+        private Dictionary<string, int> _collection;
+ 
+        public Digest(string ignorePattern, string sanitizationPattern)
+        {
+            if (!string.IsNullOrWhiteSpace(ignorePattern))
+            {
+                _ignoreMatcher = new Regex(ignorePattern);
+            }
+
+            if (!string.IsNullOrWhiteSpace(sanitizationPattern))
+            {
+                _sanitizationMatcher = new Regex(sanitizationPattern);
+            }
+
+            _collection = new Dictionary<string, int>();
+        }
+
+        public void Add(string prospect)
+        {
+            if (_ignoreMatcher != null && _ignoreMatcher.IsMatch(prospect)) return;
+            var sanitizedProspect = _sanitizationMatcher != null ? _sanitizationMatcher.Replace(prospect, "") : prospect;
+            if (sanitizedProspect.Length == 0) return;
+
+            if (_collection.ContainsKey(sanitizedProspect))
+            {
+                _collection[sanitizedProspect] = _collection[sanitizedProspect] + 1;
+            }
+            else
+            {
+                _collection[sanitizedProspect] = 1;
+            }
+        }
+
+        public IEnumerable<object> GetDigest()
+        {
+            return _collection.Select(e => new { Url = e.Key, Count = e.Value }).OrderByDescending(e => e.Count);
+        }
+
+        public void Clear()
+        {
+            _collection = new Dictionary<string, int>();
+        }
+    }
+}
---
+++ Seq.App.EventFieldDigest/EventFieldDigestReactor.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using System.Timers;
+using Seq.Apps;
+using Seq.Apps.LogEvents;
+
+namespace Seq.App.EventFieldDigest
+{
+    [SeqApp("EventFieldDigest", Description = "Creates a digest event of a event types unique field value for a defined time intervall.")]
+    public class EventFieldDigestReactor : Reactor, ISubscribeTo<LogEventData>
+    {
+        private Timer _timer;
+        private Digest _digest;
+        
+        [SeqAppSetting(DisplayName = "Field name", HelpText = "Field name to digest values from")]
+        public string FieldName { get; set; }
+
+        [SeqAppSetting(DisplayName = "Interval", HelpText = "How many hours should event field data be collected before digest is generated", InputType = SettingInputType.Integer)]
+        public int Interval { get; set; }
+
+        [SeqAppSetting(DisplayName = "Ignore pattern", HelpText = "Field values that match this pattern will be ignored", IsOptional = true)]
+        public string IgnorePattern { get; set; }
+
+        [SeqAppSetting(DisplayName = "Sanitize pattern", HelpText = "Parts of field value that match this pattern will be removed", IsOptional = true)]
+        public string SanitizePattern { get; set; }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            _digest = new Digest(IgnorePattern, SanitizePattern);
+            _timer = new Timer(1000 * Interval);
+            _timer.Elapsed += CreateDigestEvent;
+            _timer.AutoReset = true;
+            _timer.Start();
+        }
+
+        public async void On(Event<LogEventData> evt)
+        {
+            if (evt.Data.Properties.ContainsKey(FieldName))
+            {
+                _digest.Add(evt.Data.Properties[FieldName].ToString());
+            }
+        }
+
+        private void CreateDigestEvent(object obj, ElapsedEventArgs args)
+        {
+            _timer.Stop();
+            var data = _digest.GetDigest();
+            _digest.Clear();
+
+            if (data.Any())
+            {
+                
+                Log
+                    .ForContext("Digest", data, true)
+                    .ForContext("FieldName", FieldName)
+                    .Information("Field digest for {0}", FieldName);
+            }
+
+            _timer.Start();
+        }
+    }
+}
